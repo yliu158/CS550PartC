75cae4406fd1ca8db77b78cbca5ed7b8a5416fba
Yang Liu <yliu158@binghamton.edu>
2017-10-22 19:36:54 -0400

    change to user file

--- partC.c
+++ partC.c
@@ -1,22 +1,30 @@
-truct timeval gtodTimes[N];
+#define N 100
+
+struct timeval gtodTimes[N];
 char *procClockTimes[N];
 
-...
-/* allocate memory for character buffers HERE before you use them */
-
-fd = open("/dev/mytime", O_RDONLY);
-/* check for errors HERE */
-
-for( i=0; i < N; i++)
-{
-	gettimeofday(&gtodTimes[i], 0);
-	bytes_read = read(fd, procClockTime[i], MAX_LENGTH);
+int main(){
+	
+	/* allocate memory for character buffers HERE before you use them */
+	fd = open("/dev/mytime", O_RDONLY);
 	/* check for errors HERE */
-}
 
-close(fd);
+	for( i=0; i < N; i++)
+	{
+		gettimeofday(&gtodTimes[i], 0);
+		bytes_read = read(fd, procClockTime[i], MAX_LENGTH);
+		/* check for errors HERE */
+		if (bytes_read == 0) {
+			printf("Successfully read.\n");
+		} else {
+			printf("Fail to read.\n");
+		}
+	}
+	
+	close(fd);
 
-for(i=0; i < N; i++) {
-	printf("...", gtodTimes[i], procClockTime[i]);
-	/* fix the output format appropriately in the above line */
+	for(i=0; i < N; i++) {
+		printf("", gtodTimes[i], procClockTime[i]);
+		/* fix the output format appropriately in the above line */
+	}	
 }
